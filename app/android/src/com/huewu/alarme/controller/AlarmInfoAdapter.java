d9460e095fd31ec713c24ac4965246e0fbf0c3cd
Chansuk Yang <huewu@Chansuk-ui-MacBook-Pro.local>
2012-10-06 14:13:40 +0900

    fix ui

---
+++ app/android/src/com/huewu/alarme/controller/AlarmInfoAdapter.java
@@ -0,0 +1,39 @@
+package com.huewu.alarme.controller;
+
+import com.huewu.alarme.R;
+import com.huewu.alarme.model.AlarmInfo;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+public class AlarmInfoAdapter extends ArrayAdapter<AlarmInfo>{
+
+	public AlarmInfoAdapter(Context context, int textViewResourceId) {
+		super(context, textViewResourceId);
+	}
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		if( convertView == null ){
+			convertView = initView();
+		}
+		setUpView();
+		return convertView;
+	}
+
+	private void setUpView() {
+		
+	}
+
+	private View initView() {
+
+		LayoutInflater inflater = LayoutInflater.from(getContext());
+		View view = inflater.inflate(R.layout.alarm_item, null);
+		
+		return view;
+	}
+
+}//end of class
